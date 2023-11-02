7de7f5163c3ea510e06592214c65a382e8d8468f
ManuJ90 <juan.scagni@gmail.com>
2023-11-02 12:50:21 -0300

    better transition between themes

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  var isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  var bodyElement = document.body;
-  var themeMetaTag = document.querySelector('meta[name="theme-color"]');
-
-  if (isDarkMode) {
-    bodyElement.classList.add('dark');
-    themeMetaTag.setAttribute('content', '#colorForDarkMode');
+document.getElementById('theme-toggle').addEventListener('click', function () {
+  let themeStyle = document.getElementById('theme-style');
+  if (themeStyle.getAttribute('href') == '/style/dark-style.css') {
+    themeStyle.href = '/style/light-style.css';
   } else {
-    bodyElement.classList.add('light');
-    themeMetaTag.setAttribute('content', '#colorForLightMode');
+    themeStyle.href = '/style/dark-style.css';
   }
 });
-
-function toggleMode() {
-  var bodyElement = document.body;
-  var themeMetaTag = document.querySelector('meta[name="theme-color"]');
-
-  if (bodyElement.classList.contains('dark')) {
-    bodyElement.classList.remove('dark');
-    bodyElement.classList.add('light');
-    themeMetaTag.setAttribute('content', '#colorForLightMode');
-  } else {
-    bodyElement.classList.remove('light');
-    bodyElement.classList.add('dark');
-    themeMetaTag.setAttribute('content', '#colorForDarkMode');
-  }
-}
