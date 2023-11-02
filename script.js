6e2a1982c87690d74a8ff1cd93c96d8f535e5189
Juan Scagni <juan@Juans-MBP.fibertel.com.ar>
2023-11-02 03:10:59 -0300

    dark mode

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   var isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
   var bodyElement = document.body;
+  var themeMetaTag = document.querySelector('meta[name="theme-color"]');
+
   if (isDarkMode) {
     bodyElement.classList.add('dark');
+    themeMetaTag.setAttribute('content', '#colorForDarkMode');
   } else {
     bodyElement.classList.add('light');
+    themeMetaTag.setAttribute('content', '#colorForLightMode');
   }
 });
 
 function toggleMode() {
   var bodyElement = document.body;
+  var themeMetaTag = document.querySelector('meta[name="theme-color"]');
+
   if (bodyElement.classList.contains('dark')) {
     bodyElement.classList.remove('dark');
-    console.log('quiero remover esto');
     bodyElement.classList.add('light');
+    themeMetaTag.setAttribute('content', '#colorForLightMode');
   } else {
     bodyElement.classList.remove('light');
-    console.log('quiero que se oscuro');
     bodyElement.classList.add('dark');
+    themeMetaTag.setAttribute('content', '#colorForDarkMode');
   }
 }
