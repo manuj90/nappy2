9366dc30d70f8a41f154e113cb0e471e43ff3a51
Juan Scagni <juan@Juans-MBP.fibertel.com.ar>
2023-11-04 10:48:50 -0300

    navbar

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.getElementById('theme-toggle').addEventListener('click', function () {
   let themeStyle = document.getElementById('theme-style');
+  let themeImage = document.querySelector('#theme-toggle img');
+  let themeImageWebP = document.querySelector(
+    '#theme-toggle source[type="image/webp"]'
+  );
+  let themeImageJPEG = document.querySelector(
+    '#theme-toggle source[type="image/jpeg"]'
+  );
+
   if (themeStyle.getAttribute('href') == '/style/dark-style.css') {
     themeStyle.href = '/style/light-style.css';
+    themeImageWebP.srcset = '/assets/moon.webp';
+    themeImageJPEG.srcset = '/assets/moon.png';
+    themeImage.src = '/assets/moon.png';
+    themeImage.alt = 'Switch to dark theme';
   } else {
     themeStyle.href = '/style/dark-style.css';
+    themeImageWebP.srcset = '/assets/sun.webp';
+    themeImageJPEG.srcset = '/assets/sun.png';
+    themeImage.src = '/assets/sun.png';
+    themeImage.alt = 'Switch to light theme';
   }
 });
